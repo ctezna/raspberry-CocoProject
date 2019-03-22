1617556980becb750f63ebec88c42540dd4c513b
ctezna <cteznas@eafit.edu.co>
2019-03-22 01:41:44 -0500

    static

--- server/cocoProject/routes.py
+++ server/cocoProject/routes.py
@@ -4,3 +4,11 @@ from cocoProject import app, db
 @app.route("/")
 def init():
     return "Welcome to The Coco Project IoT module."
+
+@app.route("/feed")
+def feed():
+    return "This will activate the food dispenser."
+
+@app.route("/camera")
+def camera():
+    return "This will activate the live stream."
