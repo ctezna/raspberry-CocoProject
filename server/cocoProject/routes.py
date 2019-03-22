4014503f77923b02783c63f50534a331722ad0cd
ctezna <cteznas@eafit.edu.co>
2019-03-22 01:38:50 -0500

    static

--- server/cocoProject/routes.py
+++ server/cocoProject/routes.py
@@ -1,6 +1,6 @@
 from cocoProject import app, db
-from cocoProject.static import Logo_big.png
+
 
 @app.route("/")
 def init():
-    return Logo_big.png
+    return "Welcome to The Coco Project IoT module."
