a29b3c734edfac9cf74eeb8ee31bbcd7d6a703d7
ctezna <cteznas@eafit.edu.co>
2019-03-28 21:18:37 -0500

    camera module

--- server/cocoProject/routes.py
+++ server/cocoProject/routes.py
@@ -1,4 +1,6 @@
 from cocoProject import app, db
+from cocoProject.camera_pi import Camera
+from flask import render_template, Response
 
 
 @app.route("/")
@@ -9,6 +11,15 @@ def init():
 def feed():
     return "This will activate the food dispenser."
 
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route("/camera")
 def camera():
-    return "This will activate the live stream."
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
