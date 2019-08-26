fe40174bcf44afb6d93cca0016c15c7ad5bccee3
ctezna <cteznas@eafit.edu.co>
2019-08-26 01:55:37 -0500

    light switch

--- server/cocoProject/light_control.py
+++ server/cocoProject/light_control.py
@@ -13,7 +13,7 @@ class LightControl():
 
     def _thread(self, red, green, blue, brightness):
         lightFile = os.path.join("cocoProject", "lights", "lightController.py")
-        cmd = "sudo python3 " + lightFile + str(red) + " "\
+        cmd = "sudo python3 " + lightFile + " " +  str(red) + " "\
              + str(green) + " " + str(blue) + " " + str(brightness)
         if red < 0 and green < 0 and blue < 0:
             lightFile = os.path.join("cocoProject", "lights", "rainbow.py")
