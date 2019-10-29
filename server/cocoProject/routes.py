9d4275a4f7c9f735dad9e2a240e0e3b0b7be4950
ctezna <cteznas@eafit.edu.co>
2019-10-29 00:39:53 -0500

    light status json file

--- server/cocoProject/routes.py
+++ server/cocoProject/routes.py
@@ -82,8 +82,8 @@ def light():
 def light_status():
     lightstat = '/home/pi/raspberry-cocoproject/server/cocoProject/lightstatus.json'
     data = ''
-    with open(lightstat) as json:
-        data = json.load(json)
+    with open(lightstat) as json-file:
+        data = json.load(json-file)
     light = Light.query.first()
     return jsonify(data), 200
 
