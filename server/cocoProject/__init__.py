090c9a319940b878faab257a52f5e8416e5e3e32
teznatech <teznatech@gmail.com>
2019-11-14 21:33:19 -0500

    update

--- server/cocoProject/__init__.py
+++ server/cocoProject/__init__.py
@@ -8,13 +8,6 @@ from cocoProject.motor_control import MotorControl
 import os, threading
 from time import sleep
 
-def _thread_app():
-    os.system('sudo $(which python3) /home/pi/IoT-Bootcamp/Project1/code/station.py > \
-        /home/pi/logs/tutorial.log 2>&1 &')
-
-def _thread_ngrok():
-    os.system('sudo $(which ngrok) http -subdomain=ctezna 5000 > \
-        /home/pi/logs/ngrok.log 2>&1 &')
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -29,8 +22,6 @@ if Config.DEVICETYPE == 'coco':
     lightControl.lightSwitch(-2, -2, -2, 0.2)
     lightControl.lightSwitch(0, 0, 0, 0)
 elif Config.DEVICETYPE == 'horus':
-    threading.Thread(target=_thread_app).start()
-    threading.Thread(target=_thread_ngrok).start()
     motor = 404
 
 from cocoProject import routes, models
